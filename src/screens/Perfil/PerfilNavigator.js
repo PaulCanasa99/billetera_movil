523dd41136937e511e21ee2848734ee390e147df
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2021-03-08 20:14:54 -0500

    Vista de Mis tarjetas creada

--- src/screens/Perfil/PerfilNavigator.js
+++ src/screens/Perfil/PerfilNavigator.js
@@ -3,7 +3,10 @@ import { createStackNavigator } from "@react-navigation/stack";
 import CustomNavigationBar from "../../components/CustomNavigationBar";
 import { Appbar, useTheme } from "react-native-paper";
 import { StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 import Perfil from "./Perfil";
+import Activity from "../Actividad/Activity";
+import TarjetaAsociada from "./TarjetasAsociadas";
 
 const PerfilNavigator = () => {
   const Stack = createStackNavigator();
@@ -22,13 +25,25 @@ const PerfilNavigator = () => {
                 style={style.appTitle}
                 title={scene.route.name}
               />
-              {previous ? <Appbar.Action /> : null}
+              {scene.route.name === "Mi Perfil" ? (
+                <Appbar.Action
+                  icon="plus"
+                  size={35}
+                  onPress={() => {
+                    navigation.navigate("Mis tarjetas");
+                  }}
+                />
+              ) : (
+                <Appbar.Action />
+              )}
             </Appbar.Header>
           );
         },
       }}
     >
       <Stack.Screen name="Mi Perfil" component={Perfil} />
+      <Stack.Screen name="Tus movimientos" component={Activity} />
+      <Stack.Screen name="Mis tarjetas" component={TarjetaAsociada} />
     </Stack.Navigator>
   );
 };
